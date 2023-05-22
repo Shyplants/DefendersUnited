76c212f32fc517c6a4665cbd0c98a5577594e186
Shyplants <wlsdn1004100@naver.com>
2023-05-22 22:59:23 +0900

    발사체 연동
    rpc를 통해 모든 클라에서 발사체 확인 가능

--- Source/DefendersUnited/DUComponent/CombatComponent.h
+++ Source/DefendersUnited/DUComponent/CombatComponent.h
@@ -32,10 +32,10 @@ protected:
 	void FireButtonPressed(bool bPressed);
 
 	UFUNCTION(Server, Reliable)
-	void ServerFire();
+	void ServerFire(const FVector_NetQuantize& TraceHitTarget);
 
 	UFUNCTION(NetMulticast, Reliable)
-	void MulticastFire();
+	void MulticastFire(const FVector_NetQuantize& TraceHitTarget);
 
 	void TraceUnderCrosshairs(FHitResult& TraceHitResult);
 
@@ -55,8 +55,6 @@ private:
 	float AimWalkSpeed;
 
 	bool bFireButtonPressed;
-
-	FVector HitTarget;
 
 public:
 
